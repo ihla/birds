6bd36b656fe4537853fb3b050cb5f927900fc880
ihla <lubos.ilcik@gmail.com>
2013-10-29 10:33:20 +0100

    - animation params

--- Birds/src/com/guerillacraft/birds/Birds.java
+++ Birds/src/com/guerillacraft/birds/Birds.java
@@ -3,14 +3,24 @@ package com.guerillacraft.birds;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Matrix4;
 
 public class Birds implements ApplicationListener {
+	private static final int NUMBER_OF_FRAMES = 10;
+	private static final String SPRITE_NAME = "vtak_modry/";
+	private static final String TAG = "Birds";
 	private SpriteBatch batch;
 	private TextureAtlas atlas;
+	private AtlasRegion[] atlasRegion;
+	private Animation flyingBird;
+	private float stateTime;
+	private TextureRegion currentFrame;
 	private Sprite sprite;
 	
 	@Override
@@ -23,6 +33,13 @@ public class Birds implements ApplicationListener {
 
 		atlas = new TextureAtlas(Gdx.files.internal("data/blue_bird.txt"), Gdx.files.internal("data/"));
 		sprite = atlas.createSprite("vtak_modry/0000");
+		atlasRegion = new AtlasRegion[NUMBER_OF_FRAMES];
+		for (int frameIdx = 0; frameIdx < NUMBER_OF_FRAMES; frameIdx++) {
+			atlasRegion[frameIdx] = atlas.findRegion(SPRITE_NAME + String.format("%04d", frameIdx));
+			//Gdx.app.log(TAG, SPRITE_NAME + String.format("%04d", frameIdx));
+		}
+		flyingBird = new Animation(1f / NUMBER_OF_FRAMES, atlasRegion);
+		stateTime = 0;
 	}
 
 	@Override
@@ -36,8 +53,12 @@ public class Birds implements ApplicationListener {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
+		stateTime += Gdx.graphics.getDeltaTime();
+		currentFrame = flyingBird.getKeyFrame(stateTime, true);
+		
 		batch.begin();
-		sprite.draw(batch);
+		//sprite.draw(batch);
+		batch.draw(currentFrame, 50, 50);
 		batch.end();
 	}
 
